b1f98ecc1be1275917e1ee2e5cbfcc99cf0cd973
Charisto Marc Gybran <marcgybran07@gmail.com>
2022-09-01 00:15:58 +0700

    buat style tipis2

--- TemplateSoal.php
+++ TemplateSoal.php
@@ -5,18 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Soal</title>
-    <link rel="stylesheet" href="./style/style.css">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    <section id="template">
-        <h4>Soal no <?= $_GET["id"]?></h4   >
-        <h2><?= $_GET["soal"]?></h2>
-        <p>Pilihan Jawaban</p>
-        <input type="radio" name="pilihan" value="<?= $_GET['pilihan1'] ?>"><?= $_GET["pilihan1"] ?></input><br/>
-        <input type="radio" name="pilihan" value="<?= $_GET['pilihan2'] ?>"><?= $_GET["pilihan2"] ?></input><br/>
-        <input type="radio" name="pilihan" value="<?= $_GET['pilihan3'] ?>"><?= $_GET["pilihan3"] ?></input><br/>
-        <input type="radio" name="pilihan" value="<?= $_GET['pilihan4'] ?>"><?= $_GET["pilihan4"] ?></input><br/>   
-    </section>
+    <div class="container">
+        <h2 class="number"><?= $_GET['id']?></h2>
+
+        <section>
+            <div class="question-text">
+                <h2><?= $_GET['soal']?> </h2>
+            </div>
+    
+            <div class="option-list">
+                <div class="option">
+                    <input type="radio" name="pilihan" value="<?= $_GET['pilihan1'] ?>"><?= $_GET["pilihan1"] ?></input><br/>
+                </div>
+                <div class="option">
+                    <input type="radio" name="pilihan" value="<?= $_GET['pilihan2'] ?>"><?= $_GET["pilihan2"] ?></input><br/>
+                </div>
+                <div class="option">
+                    <input type="radio" name="pilihan" value="<?= $_GET['pilihan3'] ?>"><?= $_GET["pilihan3"] ?></input><br/>
+                </div>
+                <div class="option">
+                    <input type="radio" name="pilihan" value="<?= $_GET['pilihan4'] ?>"><?= $_GET["pilihan4"] ?></input><br/>   
+                </div>
+            </div>        
+            
+        </section>
+    </div>
     
 </body>
 </html>
--- index.php
+++ index.php
@@ -64,7 +64,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tugas GET</title>
-    <link rel="stylesheet" href="./style/style.css">
+    <!-- nnt dibuat khusus aja lagi buat file ini  -->
+    <!-- <link rel="stylesheet" href="style/style.css"> -->
 </head>
 <body>
     <section id="container">
